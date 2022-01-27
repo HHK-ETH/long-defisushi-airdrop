e7db93743184fcaa8d2997a31fe224cace26176b
HHK <hhk.contact@protonmail.com>
2022-01-27 11:23:15 +0100

    feat: add natspec

--- src/LongDefiSushiAirdrop.sol
+++ src/LongDefiSushiAirdrop.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.11;
 
 import "solmate/tokens/ERC1155.sol";
@@ -10,26 +10,39 @@ struct DropData {
     string link;
 }
 
-//ERC1155 for multiple airdrops
+/// @title LongDefiSushiAirdrop
+/// @author HHK-ETH
+/// @notice Airdrop multiple erc1155 tokens to selected addresses using merkle tree
 contract LongDefiSushiAirdrop is ERC1155 {
+
+    /// Errrors
+
     error Error_NotOwner();
     error Error_DateExpired();
     error Error_InvalidAmount();
     error Error_InvalidProof();
 
+    /// Events
+
     event SetDrop(uint256 indexed id, bytes32 root, uint256 maxClaimDate, string link);
     event Claimed(uint256 indexed id, address indexed to, uint256 amount);
 
+    /// Variables
+
+    /// @notice owner of the contract
     address public owner;
-    //drop id => data
+    /// @notice drop id => data
     mapping(uint256 => DropData) internal drop;
-    //drop id => address => amount claimed
+    /// @notice drop id => address => amount claimed
     mapping(uint256 => mapping(address => uint256)) public claimed;
 
     constructor() {
         owner = msg.sender;
     }
 
+    /// Modifiers
+
+    /// @notice Modifier to make functions callable by owner only
     modifier onlyOwner() {
         if (owner != msg.sender) {
             revert Error_NotOwner();
@@ -37,12 +50,25 @@ contract LongDefiSushiAirdrop is ERC1155 {
         _;
     }
 
-    function setDrop(uint256 id, bytes32 root, uint256 maxClaimDate, string calldata link) external onlyOwner {
-        drop[id] = DropData(root, maxClaimDate, link);
+    /// State change functions
 
-        emit SetDrop(id, root, maxClaimDate, link);
+    /// @notice Edit or add new token Id and airdrop merkle root
+    /// @param id Id of the token to edit/add
+    /// @param root Merkle root of the airdrop
+    /// @param expiry Max date the token can be claimed
+    /// @param link Link to the token image/data
+    function setDrop(uint256 id, bytes32 root, uint256 expiry, string calldata link) external onlyOwner {
+        drop[id] = DropData(root, expiry, link);
+
+        emit SetDrop(id, root, expiry, link);
     }
 
+    /// @notice Claim airdrop using merkle proofs
+    /// @param id Id of the token to claim, user to compute the leaf
+    /// @param to Address to receive the airdrop, user to compute the leaf
+    /// @param amount Amount to claim, user can decide to claim less than deserved amount
+    /// @param maxAmount Total amount deserved to the user, used to compute the leaf
+    /// @param proof Array of merkle proofs to compute the root
     function claim(uint256 id, address to, uint256 amount, uint256 maxAmount, bytes32[] calldata proof) external {
         DropData memory data = drop[id];
         if (data.expiry < block.timestamp) {
@@ -63,14 +89,24 @@ contract LongDefiSushiAirdrop is ERC1155 {
         emit Claimed(id, to, amount);
     }
 
+    /// @notice Transfer ownership of the contract to a new address
+    /// @param newOwner New owner of the contract
     function transferOwnership(address newOwner) external onlyOwner {
         owner = newOwner;
     }
 
-    function uri(uint256 id) public view override returns (string memory) {
+    /// View functions
+
+    /// @notice View function to get token image/data link
+    /// @param id Id of the token
+    /// @return link Returns the link to token image/data
+    function uri(uint256 id) public view override returns (string memory link) {
         return drop[id].link;
     }
 
+    /// @notice View function to get token dropData
+    /// @param id Id of the token/airdrop
+    /// @return data Returns the DropData struct
     function dropData(uint id) public view returns (DropData memory data) {
         return drop[id];
     }
